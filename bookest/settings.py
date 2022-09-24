ad0d05782857afc42e7b45fff5067dd82a073e41
jankat <cankatguven@gmail.com>
2022-09-24 21:37:33 +0300

    add domain edit1

--- bookest/settings.py
+++ bookest/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3k4zpzn@j4-!vf6*7d%lqwo&-w+bzqj&r!g=muh+&6u_p!9$x#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "thebookest.herokuapp.com"]
 
