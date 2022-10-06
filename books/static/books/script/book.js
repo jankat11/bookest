981ad166fefae06c4f9aa10ba7f45faebc9b8154
jankat <cankatguven@gmail.com>
2022-10-06 16:28:24 +0300

    rm bug fix4

--- books/static/books/script/book.js
+++ books/static/books/script/book.js
@@ -74,7 +74,7 @@ if(document.querySelector("#removeBookShelfButton")) {
             spinner.style.display = "inline-block"
             document.querySelector("#addBookShelfButton").disabled = true
             this.disabled = true
-            document.querySelector("#buttonStatus").setAttribute("status","False")
+            document.querySelector("#buttonStatus").setAttribute("data-status","False")
             fetch(`/remove_from_bookshelf/${this.dataset.book}`)
             .then(response => response.json())
             .then(result => {
