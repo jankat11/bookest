f4d2be6bd99abfac4b8e27ce48a16dea7d0fc639
jankat <cankatguven@gmail.com>
2023-01-23 10:19:20 +0300

    csrf trust add

--- bookest/settings.py
+++ bookest/settings.py
@@ -139,3 +139,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CSRF_TRUSTED_ORIGINS = ['https://bookest-production.up.railway.app']
