a874eebe96959bfc8ecb842601e0817f326d80a0
jankat <cankatguven@gmail.com>
2023-01-23 07:28:00 +0300

    requirements renew

--- bookest/settings.py
+++ bookest/settings.py
@@ -11,12 +11,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
-
+from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = os.getenv("SECRET_KEY")
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'books/static') 
+""" STATIC_ROOT = os.path.join(BASE_DIR, 'books/static')  """
 
 
 # Default primary key field type
