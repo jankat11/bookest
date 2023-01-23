c476cf21179ca11b952ded684d10785f1427d643
jankat <cankatguven@gmail.com>
2023-01-23 22:37:25 +0300

    postgres add

--- bookest/settings.py
+++ bookest/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import dj_database_url
 import os
 from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
@@ -82,19 +82,13 @@ AUTH_USER_MODEL = "books.User"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
-    }
+"default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
-DATABASE_URL="postgresql://postgres:WT88pFT4ENHw4ltTntW8@containers-us-west-64.railway.app:7601/railway"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
