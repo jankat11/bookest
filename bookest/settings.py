dae39ff3a723c41ef2a1a1436035ba25a486d591
jankat <cankatguven@gmail.com>
2023-01-23 06:23:34 +0300

    add vercel json

--- bookest/settings.py
+++ bookest/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
-import django_heroku
 from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
